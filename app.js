50183e8d15097eb12ef5794ad862fb5ed9b6ac5d
Jacklen <jacklen@Jacklens-MacBook-Pro.local>
2019-12-02 00:13:58 +0530

    fix

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const bodyParser = require('body-parser')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,18 @@ const Database = require('../database/database')
 const User =  require("../database/model/user")
 // const connection = new Database;
 /* GET home page. */
+function onSuccess(event)
+{
+  console.log('onSuccess:',event)
+  return event
+}
+
+function onError(event)
+{
+  console.log('onError:',event)
+}
+
+
 router.get('/', function(req, res, next) {
   let userObj = new User();
   userObj.id = 2
@@ -16,17 +28,33 @@ router.get('/', function(req, res, next) {
     )}))
 
 
-res.render('index', { title: 'Express' });
+res.render('index', { title: 'Twitter Replica' });
 });
 
-function onSuccess(event)
-{
-  console.log('onSuccess',event)
-  return event
-}
 
-function onError(event)
-{
-  console.log('onSuccess',event)
-}
+router.post('/twit',(req,res,next)=>{
+
+  console.log(req.body.exampleInputEmail1)
+  if(req.body.exampleInputEmail1)
+  {
+    let userObj = new User()
+    userObj.id = 3
+    userObj.name=req.body.exampleInputEmail1
+    Database.then(async(con)=>{
+      twit = await con.getRepository(User).createQueryBuilder("user").getMany() // .then(onSuccess,onError)
+      console.log("ttt",twit)
+      res.render('index',{ title: 'Twitter Replica', twitposts: twit})
+
+      })
+    // console.log('Database:',Database.then(async(con)=>{console.log(
+    //   await con.manager.save(userObj).then(onSuccess,onError)
+    //    )}))
+    
+  }
+  else
+  res.end('sorry no posts for you')
+  
+})
+
+
 module.exports = router;
